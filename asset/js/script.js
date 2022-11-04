3eb7cbb493e10cbe6ee454929d7934a4517f4c71
jessicashong <jessicashong12@gmail.com>
2022-11-04 17:35:41 -0500

    updated styling and javascript

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,7 @@
 //title page
 var timer = document.getElementById('timer');
 var highScores = document.getElementById('highscores');
+var startPage = document.getElementById('start-page');
 var title = document.querySelector('.title');
 var directions = document.querySelector('.directions');
 var startBtn = document.getElementById('start');
@@ -8,62 +9,112 @@ var startBtn = document.getElementById('start');
 var questionsContainer = document.getElementById('question-container');
 var btnContainer = document.getElementById('btn-container');
 var question = document.getElementById('question');
-var choice1 = document.getElementById('choice1');
-var choice2 = document.getElementById('choice2');
-var choice3 = document.getElementById('choice3');
-var choice4 = document.getElementById('choice4');
+// var choiceBtn = document.querySelector('.choice-btn');
+// var choice2 = document.getElementById('choice2');
+// var choice3 = document.getElementById('choice3');
+// var choice4 = document.getElementById('choice4');
 // var choiceContainer = document.querySelector('.choice-container');
 
 var score = 0;
+var index = 0;
 
 
 title.textContent = 'Code Quiz';
 directions.textContent = 'Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!';
-title.setAttribute('style', 'font-size:40px; text-align:center;');
+title.setAttribute('style', 'font-size:45px; text-align:center;');
 directions.setAttribute('style', 'font-size:20px; text-align:center');
 startBtn.setAttribute('style', 'margin:20px');
-startBtn.addEventListener('click', startGame());
+
+startBtn.addEventListener('click', startGame);
 
 function startGame(){
     //change id names and remove p tag
-    startBtn.classList.add('hide');
-    title.classList.add('hide');
-    directions.classList.add('hide');    
+    startPage.classList.add('hide');
+    startPage.classList.remove('start-page');
+    questionsContainer.classList.remove('hide');
+    questionsContainer.classList.add('question-container');
+
+    questionsStart();
+    startTimer();
 };
 
+function startTimer(){
+    var timeLeft = 5;
+    var timeInterval = setInterval(function(){
+        if (timeLeft > 0) {
+            timer.innerHTML = 'Timer: ' + timeLeft;
+            timeLeft--;
+        } else {
+            timer.innerHTML = 'Timer: 0';
+            clearInterval(timeInterval);
+
+            displayScore();
+        }
+    }, 1000)
+}
 //create arrays with question and answer obejcts
 var questionBank = [
     {
         question: 'Commonly used data types DO NOT include:',
-        answers: ['strings', 'booleans', 'alerts', 'numbers']
+        answers: [
+            { text: 'strings', correct: false},
+            { text: 'booleans', correct: false},
+            { text: 'alerts', correct: true},
+            { text: 'numbers', correct: false}
+        ]
     },
     {
-        question: 'are you hungry?',
-        answers: ['no', 'yes', 'kinda']
+        question: 'how old am i?',
+        answers: [
+            { text: '12', correct: false},
+            { text: '27', correct: true},
+            { text: '22', correct: false},
+            { text: '37', correct: false}
+        ]
     },
+    {
+        question: 'what is my favorite color?',
+        answers: [
+            { text: 'blue', correct: false},
+            { text: 'red', correct: false},
+            { text: 'green', correct: false},
+            { text: 'yellow', correct: true}
+        ]
+    },
+    {
+        question: 'what is my favorite food?',
+        answers: [
+            { text: 'poke', correct: true},
+            { text: 'sushi', correct: false},
+            { text: 'tacos', correct: false},
+            { text: 'hamburger', correct: false}
+        ]
+    }
 ]
 
 //function that creates ul tag and buttons
-function questionsPage(){
-    var questions = document.querySelector('.questions');
-    var ulEl = document.createElement('ul');
-    choiceContainer.appendChild(ulEl);
+function questionsStart(){
+    question.innerText = questionBank[index].question;
     
-    // for (var i = 0; i < questionBank.length; i++){
-    //     question.textContent = questionBank[i].question;
-    //     var response = 
-    //     //if (question ==
-    // }
-    questions.textContent = questionBank[0].question;
-    questions.setAttribute('style', 'font-size:30px; text-align:center');
-    // for (var i = 0; i < questionBank[].answers.length; i++){
-    //     var choice = document.createElement('button');
-    //     choice.textContent = questionBank[i].answers[n];
-    // }
+    for (var i = 0; i < questionBank[index].answers.length; i++){
+        var choice = document.createElement('button');
+        choice.classList.add('choice-btn');
+        choice.textContent = questionBank[index].answers[i].text;
+        btnContainer.appendChild(choice);
+    }
+    if (answer.correct) {
+        choice.dataset.correct = answer.correct;
+    }
+    index ++;
 }
 
-function selectAnswer(){
+function nextQuestion(){
+    question.innerText = questionBank[index].question;
+    
+}
 
+function displayScore(){
+    
 }
 // var questions = document.querySelector('.questions');
 // var ulEl = document.createElement('ul');
