ed6fe587c89f3a19b3f152aed3c96d942f90f1db
jessicashong <jessicashong12@gmail.com>
2022-11-03 21:39:37 -0500

    updated javascript

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,7 +2,8 @@
 var title = document.querySelector('.title');
 var questionsContainer = document.querySelector('.content');
 var directions = document.querySelector('.directions');
-var buttonContainer = document.querySelector('.button-container');
+//var buttonContainer = document.querySelector('.button-container');
+var choiceContainer = document.querySelector('.choice-container');
 var startBtn = document.createElement('button');
 
 
@@ -12,8 +13,7 @@ startBtn.textContent = "START";
 title.setAttribute('style', 'font-size:40px; text-align:center;');
 directions.setAttribute('style', 'font-size:20px; text-align:center');
 
-directions.appendChild(buttonContainer);
-buttonContainer.appendChild(startBtn);
+directions.appendChild(startBtn);
 
 startBtn.setAttribute('style', 'margin:20px');
 
@@ -23,18 +23,17 @@ startBtn.addEventListener('click', function(){
     title.removeAttribute('style', 'font-size:40px; text-align:center;');
     title.classList.replace('title', 'questions');
     directions.remove();    
-    buttonContainer.remove();
+    startBtn.remove();
 
-    // questionsPage();
+    questionsPage();
     console.log(questionBank[0])
 });
-
 
 //create arrays with question and answer obejcts
 var questionBank = [
     {
-        question: 'how are you?',
-        answers: ['fine', 'alright', 'horrible']
+        question: 'Commonly used data types DO NOT include:',
+        answers: ['strings', 'booleans', 'alerts', 'numbers']
     },
     {
         question: 'are you hungry?',
@@ -44,38 +43,39 @@ var questionBank = [
 
 //function that creates ul tag and buttons
 function questionsPage(){
+    var questions = document.querySelector('.questions');
     var ulEl = document.createElement('ul');
+    choiceContainer.appendChild(ulEl);
 
-    for (var i = 0; i < questionBank.length; i++){
-        var question = document.querySelector('.questions');
-        question.textContent = questionBank[i].question;
-
-        for (var n = 0; n < questionBank[i].answers.length; n++){
-            var choice = document.createElement('button');
-            choice.textContent = questionBank[i].answers[n];
-            
-        }
+    questions.textContent = questionBank[0].question;
+    questions.setAttribute('style', 'font-size:30px; text-align:center');
+    // for (var i = 0; i < questionBank[].answers.length; i++){
+    //     var choice = document.createElement('button');
+    //     choice.textContent = questionBank[i].answers[n];
+    // }
 }
-    // var questions = document.querySelector('.questions');
-    // var ulEl = document.createElement('ul');
-    // var choice1 = document.createElement('button');
-    // var choice2 = document.createElement('button');
-    // var choice3 = document.createElement('button');
-    // var choice4 = document.createElement('button');
+// var questions = document.querySelector('.questions');
+// var ulEl = document.createElement('ul');
+// var choice1 = document.createElement('button');
+// var choice2 = document.createElement('button');
+// var choice3 = document.createElement('button');
+// var choice4 = document.createElement('button');
 
-    // choice1.textContent = questionBank[0].answer1;
-    // choice2.textContent = questionBank[0].answer1;
-    // choice3.textContent = questionBank[0].answer1;
-    
-
-    // buttonContainer.appendChild(ulEl);
-    // ulEl.appendChild(choice1);
-    // ulEl.appendChild(choice2);
-    // ulEl.appendChild(choice3);
-    // ulEl.appendChild(choice4);
+// choice1.textContent = questionBank[0].answer1;
+// choice2.textContent = questionBank[0].answer1;
+// choice3.textContent = questionBank[0].answer1;
 
 
-}
+// buttonContainer.appendChild(ulEl);
+// ulEl.appendChild(choice1);
+// ulEl.appendChild(choice2);
+// ulEl.appendChild(choice3);
+// ulEl.appendChild(choice4);
+
+
+// for (var i = 0; i < questionBank.length; i++){
+//     var question = document.querySelector('.questions');
+//     question.textContent = questionBank[i].question;
 
 
 //event listener correct answer
